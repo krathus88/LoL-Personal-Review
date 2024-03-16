e7556c1c48d6e2f40ea33a56436e0401f4961c47
krathus88 <marcoribeiro4@hotmail.com>
2024-03-16 23:13:55 +0000

    Updated Base structure and added base html + css

--- LoLPersonalReview/settings.py
+++ LoLPersonalReview/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'LoLPersonalReview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- LoLPersonalReview/urls.py
+++ LoLPersonalReview/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home_page)
 ]
