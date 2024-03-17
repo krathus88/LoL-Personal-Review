07badc405a880e5a13b85e6d5862eedac473653b
Ricardo Guegan <guegan.ricardo@gmail.com>
2024-03-17 20:19:19 +0000

    [DEV: API call início Summoner Name]

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
+import os 
+from utilFunctions import functions
+
 
 # Create your views here.
 def home_page(request):
+    api_request= functions.url_find_summoner("europe", "Hide on bush", "QYBB")
+    api_result = requests.get(api_request)
+    print (api_result)
     return render(request, "landingPage.html")
