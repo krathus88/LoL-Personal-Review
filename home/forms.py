a78e46cd8db8f0a829e88ccabe86eebfac808c6c
krathus88 <marcoribeiro4@hotmail.com>
2024-03-19 18:32:58 +0000

    Added functionality to find account and summoner info

--- home/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class SummonerForm(forms.Form):
-    region = forms.CharField(label="Region")
-    summoner_name = forms.CharField(label="Summoner Name")
-    summoner_tag = forms.CharField(label="Summoner Tag")
-    
--- utilFunctions/functions.py
+++ utilFunctions/functions.py
@@ -1,16 +1,50 @@
 import os
 import requests
 
-def url_find_summoner(server, summoner_name, tag):
-	url=os.getenv("API_URL_SUMMONER_NAME")
-	url = url.replace("server",server)
-	url = url.replace("summoner_name",summoner_name)
-	url = url.replace("tag", tag)
-	url = url + '?api_key=' + os.getenv("API_KEY")
-	api_result = requests.get(url)
+def find_account(server, summoner_name, tag):
+	dict = {
+		"EUW": "europe",
+		"EUNE": "europe",
+		"NA": "americas",
+	}
+	api_url = os.getenv("API_URL").replace("[server]", dict[server])
+	endpoint_url = os.getenv("ACCOUNT_SEARCH").replace("[gameName]", summoner_name).replace("[tagLine]", tag)
+	api_result = requests.get(api_url + endpoint_url + '?api_key=' + os.getenv("API_KEY"))
 	if api_result.status_code == 200:
 		# API call successful
 		return api_result.json()
 	else:
 		# API call failed
+		return f"Failed to retrieve summoner information. Error code: {api_result.status_code}"
+
+def find_account_id(server, puuid):
+	dict = {
+		"EUW": "euw1",
+		"EUNE": "eun1",
+		"NA": "na1",
+	}
+	api_url = os.getenv("API_URL").replace("[server]", dict[server])
+	endpoint_url = os.getenv("ACCOUNT_SUMMONER_SEARCH").replace("[encryptedPUUID]", puuid)
+	api_result = requests.get(api_url + endpoint_url + '?api_key=' + os.getenv("API_KEY"))
+	if api_result.status_code == 200:
+        # API call successful
+		return api_result.json()
+	else:
+        # API call failed
+		return f"Failed to retrieve summoner information. Error code: {api_result.status_code}"
+
+def find_summoner(server, summonerId):
+	dict = {
+		"EUW": "euw1",
+		"EUNE": "eun1",
+		"NA": "na1",
+	}
+	api_url = os.getenv("API_URL").replace("[server]", dict[server])
+	endpoint_url = os.getenv("SUMMONER_SEARCH").replace("[encryptedSummonerId]", summonerId)
+	api_result = requests.get(api_url + endpoint_url + '?api_key=' + os.getenv("API_KEY"))
+	if api_result.status_code == 200:
+        # API call successful
+		return api_result.json()
+	else:
+        # API call failed
 		return f"Failed to retrieve summoner information. Error code: {api_result.status_code}"
