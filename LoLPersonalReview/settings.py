fc4501147fa77f9a79d74d771be6758252eddf28
krathus88 <marcoribeiro4@hotmail.com>
2024-04-05 16:08:31 +0100

    [DEV]: Small Cors origins fix + Added proxy URL to package.json

--- LoLPersonalReview/settings.py
+++ LoLPersonalReview/settings.py
@@ -133,4 +133,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React endpoint
+]
